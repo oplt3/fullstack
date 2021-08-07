2d4b9e24569acd1e186590311915b5af440905f5
oplt3 <85621168+oplt3@users.noreply.github.com>
2021-08-07 16:28:31 +0200

    Add files via upload

---
+++ fullstackgit/backend/src/build/precache-manifest.6d874981c8714da4e79bf53edcaf12ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7fb3f9e725b4c33cf9",
+    "url": "/static/css/main.ae57f1a8.chunk.css"
+  },
+  {
+    "revision": "8e7fb3f9e725b4c33cf9",
+    "url": "/static/js/main.b45aa9af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "337995d2bb48a0fe81e6",
+    "url": "/static/js/2.9d2ecc14.chunk.js"
+  },
+  {
+    "revision": "f3a5801b35586a5858c9077a7b9a418d",
+    "url": "/index.html"
+  }
+];
